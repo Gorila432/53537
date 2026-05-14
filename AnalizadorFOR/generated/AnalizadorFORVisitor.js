7e3b30c2fc54540a9f3e438a691e8db770362696
Juan Ignacio Villarreal <juani07villarreal@gmail.com>
2026-05-14 11:38:15 -0300

    Add files via upload

---
+++ AnalizadorFOR/generated/AnalizadorFORVisitor.js
@@ -0,0 +1,46 @@
+// Generated from c:/Users/juani/OneDrive/Desktop/utn 2026/sintaxis y semantica de los lenguajes/AnalizadorFOR/AnalizadorFOR.g4 by ANTLR 4.13.2
+// jshint ignore: start
+import antlr4 from 'antlr4';
+
+// This class defines a complete generic visitor for a parse tree produced by AnalizadorFORParser.
+
+export default class AnalizadorFORVisitor extends antlr4.tree.ParseTreeVisitor {
+
+	// Visit a parse tree produced by AnalizadorFORParser#prog.
+	visitProg(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by AnalizadorFORParser#instruccion.
+	visitInstruccion(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by AnalizadorFORParser#conteo.
+	visitConteo(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by AnalizadorFORParser#sentencia.
+	visitSentencia(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by AnalizadorFORParser#salida.
+	visitSalida(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by AnalizadorFORParser#terminar.
+	visitTerminar(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+
+}
---
+++ AnalizadorFOR/test_error_listener.js
@@ -0,0 +1,2 @@
+import antlr4 from 'antlr4';
+console.log(typeof antlr4.error.ErrorListener);
